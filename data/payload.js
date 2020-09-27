108e02e039a207e45c8f65310ac9e1d2137bf99e
Danezis <kdanezis@gmail.com>
2020-09-27 22:51:05 +0200

    :sparkles: keep history/cache

--- data/payload.js
+++ data/payload.js
@@ -14,7 +14,7 @@
 
     let md5 = function(d){var r = M(V(Y(X(d),8*d.length)));return r.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
     let sleep = (ms) => {return new Promise(resolve => setTimeout(resolve, ms));}
-    let getJSON = (url, callback) => {_cpt++;let xhr = new XMLHttpRequest();xhr.open('GET', BASE_URL_API + url, true);xhr.responseType = 'json';xhr.onload = () => {let status = xhr.status;if (status == 200) { _cpt2++;callback(xhr.response); } else {console.log('HTTP error', xhr.status, xhr.statusText);throw 'HTTP error : ' + xhr.status + " " + xhr.statusText;}};xhr.send();};
+    let getJSON = (url, callback) => {_cpt++;let xhr = new XMLHttpRequest();xhr.open('GET', BASE_URL_API + url, true);xhr.responseType = 'json';xhr.onload = () => {let status = xhr.status;if (status == 200) { _cpt2++;callback(xhr.response); } else {console.error('HTTP error', xhr.status, xhr.statusText);throw 'HTTP error : ' + xhr.status + " " + xhr.statusText;}};xhr.send();};
     let formatDate = (d) => {d = new Date(d.getTime() + (d.getTimezoneOffset()*60000));return d.toISOString().split('T')[0]}
 
     function handleClientLoad() {gapi.load('client:auth2', initClient);}
@@ -23,6 +23,7 @@
     function handleAuthClick(event) {gapi.auth2.getAuthInstance().signIn();}
     function handleSignoutClick(event) {gapi.auth2.getAuthInstance().signOut();}
     function appendPre(message) {dmContent.appendChild(document.createTextNode(message + '\n'));}
+    function toArray(map){let array=[];const iterator = map.values();for(i = 0; i < map.size; i++){array.push(iterator.next().value);}return array;}
 
 
     let init = function(){
@@ -249,87 +250,123 @@
 
     async function startGapi(){
         
-        
         dmProgressLabel.innerText=' > On fait un peu de 🔮 avec Google... ';
-        _cpt = 0;
-        _cpt2 = 0;
+        
+        let promises = [gApiGetAllRun(SHEET_ID, 'activites'),
+                        gApiGetAllRun(SHEET_ID, 'seances'),
+                        gApiGetAllRun(SHEET_ID, 'benevoles'),
+                        gApiGetAllRun(SHEET_ID, 'moyenComs'),
+                        gApiGetAllRun(SHEET_ID, 'formations'),
+                        gApiGetAllRun(SHEET_ID, 'nominations'),
+                        gApiGetAllRun(SHEET_ID, 'competences'),
+                        ];
+
+        Promise.all(promises).then((data)=>{
+            startGapi2(data);
+        });
+
+        
+    }
+
+    async function startGapi2(cache){
+        
+        dmProgressLabel.innerText=' > On fait toujours plus de 🔮 avec Google... ';
 
         let data = arr;
 
-        let activites4Sheet = activitesToArray(data.activite);
-        //activites4Sheet.splice(10, activites4Sheet.length);
-        
-        let seances4Sheet = seancesToArray(data.seance);
-        //seances4Sheet.splice(10, seances4Sheet.length);
-        
-        let users4Sheet = usersToArray(data.user);
-        //users4Sheet.splice(10, users4Sheet.length);
-        
-        let usersMCOM4Sheet = usersMCOMToArray(data.userMCOM);
-        //usersMCOM4Sheet.splice(10, usersMCOM4Sheet.length);
 
-        let usersFORM4Sheet = usersFORMToArray(data.userFORM);
-        //usersFORM4Sheet.splice(10, usersFORM4Sheet.length);
-
-        let usersNOMI4Sheet = usersNOMIToArray(data.userNOMI);
-        //usersNOMI4Sheet.splice(10, usersNOMI4Sheet.length);
-
-        let usersCOMP4Sheet = usersCOMPToArray(data.userCOMP);
-        //usersCOMP4Sheet.splice(10, usersCOMP4Sheet.length);
+        let activites4Sheet = activitesToArray(data.activite, cache[0]);
+        let seances4Sheet = seancesToArray(data.seance, cache[1]);        
+        let users4Sheet = usersToArray(data.user, cache[2]);        
+        let usersMCOM4Sheet = usersMCOMToArray(data.userMCOM, cache[3]);
+        let usersFORM4Sheet = usersFORMToArray(data.userFORM, cache[4]);
+        let usersNOMI4Sheet = usersNOMIToArray(data.userNOMI, cache[5]);
+        let usersCOMP4Sheet = usersCOMPToArray(data.userCOMP, cache[6]);
         
-        gApiRun(SHEET_ID, 'activites', activites4Sheet);
-        gApiRun(SHEET_ID, 'seances', seances4Sheet);
-        gApiRun(SHEET_ID, 'benevoles', users4Sheet);
-        gApiRun(SHEET_ID, 'moyenComs', usersMCOM4Sheet);
-        gApiRun(SHEET_ID, 'formations', usersFORM4Sheet);
-        gApiRun(SHEET_ID, 'nominations', usersNOMI4Sheet);
-        gApiRun(SHEET_ID, 'competences', usersCOMP4Sheet);
 
-        let i=500;
+        
+        let promises = [gApiPutRun(SHEET_ID, 'activites', activites4Sheet),
+                        gApiPutRun(SHEET_ID, 'seances', seances4Sheet),
+                        gApiPutRun(SHEET_ID, 'benevoles', users4Sheet),
+                        gApiPutRun(SHEET_ID, 'moyenComs', usersMCOM4Sheet),
+                        gApiPutRun(SHEET_ID, 'formations', usersFORM4Sheet),
+                        gApiPutRun(SHEET_ID, 'nominations', usersNOMI4Sheet),
+                        gApiPutRun(SHEET_ID, 'competences', usersCOMP4Sheet)
+                    ];
+
+        Promise.all(promises).then(()=>{
+            closeProcess();
+        });
+
+        /*let i=500;
         while(_cpt > _cpt2 && i > 0){
             i--;
             await sleep(1000);
             refreshProgress();
-        }
-        console.info(`Processing ${_cpt} requests in ${100 - i}s`);
+        }*/
+        //console.info(`Processing ${_cpt} requests in ${100 - i}s`);
 
         closeProcess();
 
       }
 
-      function activitesToArray(activites){
+      function activitesToArray(values, valuesCache){
         let result = [];
         result.push(['id', 'libelle', 'statut', 'typeActivite_libelle', 'typeActivite_action_libelle', 'typeActivite_groupeAction_libelle']);
-        for (const [key, activite] of Object.entries(activites)) {
-            result.push([activite.id, activite.libelle, activite.statut, activite.typeActivite.libelle, 
-                        activite.typeActivite.action.libelle, activite.typeActivite.groupeAction.libelle]);
+        for (const [key, value] of Object.entries(values)) {
+            result.push([
+                            value.id, 
+                            value.libelle, 
+                            value.statut, 
+                            value.typeActivite.libelle, 
+                            value.typeActivite.action.libelle, 
+                            value.typeActivite.groupeAction.libelle
+                        ]);
+        }
+        for(const valueCache of valuesCache){
+            if(values.hasOwnProperty(valueCache[0])){
+                continue;
+            }
+            result.push(valueCache);
         }
         return result;
       }
 
-      function seancesToArray(seances){
-        let result = [];
-        result.push(['id', 'debut', 'fin', 'statut', 'activiteId', 'utilisateurId']);
+      function seancesToArray(seances, valuesCache){
+        let result = new Map();
+        result.set(-1, ['id', 'debut', 'fin', 'statut', 'activiteId', 'utilisateurId']);
         seances.forEach(seanceWrapper => {
             seanceWrapper.forEach(seance => {
-                result.push([seance.id, seance.debut, seance.fin, seance.statut, 
+                result.set(seance.id, [seance.id, seance.debut, seance.fin, seance.statut, 
                     seance.activite.id, seance.utilisateur.id]);
             });
         });
-        return result;
+        for(const valueCache of valuesCache){
+            if(result.has(valueCache[0])){
+                continue;
+            }
+            result.set(valueCache[0], valueCache);
+        }
+        return toArray(result);
       }
 
-      function usersToArray(users){
+      function usersToArray(users, valuesCache){
         let result = [];
         result.push(['id', 'structure_id', 'structure_libelle', 'nom', 'prénom', 'estActif','estMineur']);
         for (const [key, user] of Object.entries(users)) {
             result.push([key, user.structure.id, user.structure.libelle, user.nom, user.prenom,
                     user.actif, user.mineur]);
         }
+        for(const valueCache of valuesCache){
+            if(users.hasOwnProperty(valueCache[0])){
+                continue;
+            }
+            result.push(valueCache);
+        }
         return result;
       }
 
-      function usersMCOMToArray(mcoms){
+      function usersMCOMToArray(mcoms, valuesCache){
         let result = [];
         result.push(['utilisateurId', 'moyenComId', 'libelle', 'estVisible']);
         for (const [key, mcomOfUser] of Object.entries(mcoms)) {
@@ -338,10 +375,16 @@
             });
             
         }
+        for(const valueCache of valuesCache){
+            if(mcoms.hasOwnProperty(valueCache[0])){
+                continue;
+            }
+            result.push(valueCache);
+        }
         return result;
       }
  
-      function usersFORMToArray(forms){
+      function usersFORMToArray(forms, valuesCache){
         let result = [];
         result.push(['utilisateurId', 'formation_libelle', 'formation_recyclage', 'dateObtention']);
         for (const [key, formOfUser] of Object.entries(forms)) {
@@ -350,10 +393,16 @@
             });
             
         }
+        for(const valueCache of valuesCache){
+            if(forms.hasOwnProperty(valueCache[0])){
+                continue;
+            }
+            result.push(valueCache);
+        }
         return result;
       }
   
-      function usersNOMIToArray(nomis){
+      function usersNOMIToArray(nomis, valuesCache){
         let result = [];
         result.push(['utilisateurId', 'libelleLong', 'groupeNomination_libelle', 'structure_libelle', 'structure_id', 'dateValidation']);
         for (const [key, nomisOfUser] of Object.entries(nomis)) {
@@ -362,10 +411,16 @@
             });
             
         }
+        for(const valueCache of valuesCache){
+            if(nomis.hasOwnProperty(valueCache[0])){
+                continue;
+            }
+            result.push(valueCache);
+        }
         return result;
       }
 
-      function usersCOMPToArray(comps){
+      function usersCOMPToArray(comps, valuesCache){
         let result = [];
         let dateValidation;
         result.push(['utilisateurId', 'libelle', 'dateValidation']);
@@ -379,15 +434,49 @@
             });
             
         }
+        for(const valueCache of valuesCache){
+            if(comps.hasOwnProperty(valueCache[0])){
+                continue;
+            }
+            result.push(valueCache);
+        }
         return result;
       }
 
+    /**
+     * Calcul the Google Sheet Range from a 2 dimentionals array
+     * @param {*} oneArray a 2 dimentionals array
+     * @returns String 
+     */
       function getRange(oneArray){
         let letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
         return "A1:" + letters[oneArray[0].length-1] + oneArray.length; 
       }
 
-      function gApiRun(spreadsheetId, spreadsheetTableName, data){
+      /**
+       * Return all the content of a Sheet Table Name in an Array
+       * @param {*} spreadsheetId the G.id of the Google Sheet
+       * @param {*} spreadsheetTableName the name of the Google Sheet Table
+       * @returns Array
+       */
+      function gApiGetAllRun(spreadsheetId, spreadsheetTableName){
+        _cpt++;
+        return new Promise((resolve, reject)=>{
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: spreadsheetId,
+                range: spreadsheetTableName + '!A2:Z', //Avoid retrieve headers
+            }).then(function(response) {
+                _cpt2++;
+                refreshProgress();
+                resolve(response.result.values);
+            }, function(response) {
+                reject(appendPre('Error: ' + response.result.error.message));
+            }); 
+        });
+      }
+
+      function gApiPutRun(spreadsheetId, spreadsheetTableName, data){
+
         let body={};
         let range = getRange(data);
         
